caf226341921a0383e92c1bd700e72385684e79b
nlee100 <naomileegolfer@gmail.com>
2023-01-03 17:49:51 -0800

    start of update for new version (v2) of website. note v1 is on branch v1.

--- js/index.js
+++ js/index.js
@@ -61,7 +61,7 @@ window.addEventListener('scroll', changeNavLinkOnScroll);
 // ------------------------- console typewriter ------------------------- //
 
 // console typewriter for different noun descriptions
-consoleText(['student.','varsity athlete.', 'software engineer.', 'writer.'], 'text',['#d81159', '#956262','#4EC5C1','#F7882F']);
+consoleText(['computational biologist.', 'data engineer.', 'writer.', 'golfer.'], 'text',['#d81159', '#956262','#4EC5C1','#F7882F']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
